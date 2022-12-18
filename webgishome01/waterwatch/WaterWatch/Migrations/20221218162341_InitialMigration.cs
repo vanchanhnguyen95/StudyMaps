eea8433d92485ae181e5c6332ee90409e7db508a
DESKTOP-T0HLH7K\os <vanchanhnguyen95@gmail.com>
2022-12-18 23:26:45 +0700

    Database Migrations

---
+++ webgishome01/waterwatch/WaterWatch/Migrations/20221218162341_InitialMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WaterWatch.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consumptions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    neighbourhood = table.Column<string>(type: "text", nullable: false),
+                    suburbgroup = table.Column<string>(name: "suburb_group", type: "text", nullable: false),
+                    averageMonthlyKL = table.Column<int>(type: "integer", nullable: false),
+                    coordinates = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consumptions", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Consumptions");
+        }
+    }
+}
