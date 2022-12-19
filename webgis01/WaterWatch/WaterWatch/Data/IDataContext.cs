b8d7b6cd56edbe00be139f5ddb04131c37409ced
BA\chanhnv <vanchanhnguyen95@gmail.com>
2022-12-19 08:36:57 +0700

    Creating the Data Context Interface

---
+++ webgis01/WaterWatch/WaterWatch/Data/IDataContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WaterWatch.Models;
+
+namespace WaterWatch.Data
+{
+    public interface IDataContext
+    {
+        DbSet<WaterConsumption> Consumptions {get;set;}
+
+        int SaveChanges();
+    }
+}
