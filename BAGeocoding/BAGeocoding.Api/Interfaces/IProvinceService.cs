8e40a5f7900a93db958526709c9e11897edca792
BA\chanhnv <vanchanhnguyen95@gmail.com>
2023-06-06 09:32:08 +0700

    up

---
+++ BAGeocoding/BAGeocoding.Api/Interfaces/IProvinceService.cs
@@ -0,0 +1,13 @@
+﻿namespace BAGeocoding.Api.Interfaces;
+
+public interface IProvinceService
+{
+    Task<string> CreateIndex(string indexName);
+
+    Task<string> BulkAsync(List<Province> vietNamShapes);
+
+    //Task<string> AddAsync(List<VietNamShape> vietNamShapes);
+    // Tìm kiếm theo Tọa độ / Từ Khóa / Tọa độ và từ khóa
+    Task<List<Province>> GetDataSuggestion(int size, string keyword);
+    Task<string> GetProvinceId(string keyword);
+}
